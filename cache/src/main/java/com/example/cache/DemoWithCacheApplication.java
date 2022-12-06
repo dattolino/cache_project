4a675155e983a036e8e2b99eb699ac3a3797c17c
dattolino <50369771+dattolino@users.noreply.github.com>
2022-12-06 10:27:55 +0100

    cache_project upload

---
+++ cache/src/main/java/com/example/cache/DemoWithCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWithCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWithCacheApplication.class, args);
+	}
+
+}
